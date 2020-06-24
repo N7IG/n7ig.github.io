269975543420c429db5fbb680cf4912a824c669c
Shavel, Ihar <IShavel@slb.com>
2020-06-24 17:31:25 +0300

    line

--- index.js
+++ index.js
@@ -2,7 +2,90 @@ const COMMAND_WIDTH = 15;
 
 const canvas = document.getElementById("rec");
 const ctx = canvas.getContext("2d");
+const canvasR = document.getElementById("res");
+const ctxR = canvasR.getContext("2d");
 const input = document.getElementById("input");
+const line = document.getElementById("line");
+
+function plannedSlidesFromRecommendation(
+    recommendation,
+    takeAllStands = false
+) {
+    // only look at 1st recommendation (current drilling unit)
+    // GetRecommendations return the history of the recommendations all the way to the target
+    // Slide evaluation page & Rig floor page should only consider the 1st stand in the standRecommendations property
+    // while etude trajectory page should have all stands to display all slides
+    if (
+        !recommendation.standRecommendations ||
+        recommendation.standRecommendations.length === 0
+    ) {
+        return [];
+    }
+
+    const drillCommands = takeAllStands
+        ? _.flatten(
+              (recommendation.standRecommendations || []).map(
+                  (stand) => stand.drillCommands
+              )
+          )
+        : _.first(recommendation.standRecommendations).drillCommands;
+
+    return drillCommands
+        .filter((drillCommand) => drillCommand.drillCommandType === 2)
+        .map(drillCommandToPlannedSlideVM);
+}
+
+function plannedSlidesFromRecommendations(recommendations) {
+    return removeOutdatedIntervals(
+        _.flatten(
+            (recommendations || []).map((recommendation, index) => {
+                const isActualRecommendation =
+                    index === recommendations.length - 1;
+                return plannedSlidesFromRecommendation(
+                    recommendation,
+                    isActualRecommendation
+                );
+            })
+        )
+    );
+}
+
+function removeOutdatedIntervals(plannedSlideIntervals) {
+    return plannedSlideIntervals.reduce((filteredIntervals, nextInterval) => {
+        filteredIntervals = filteredIntervals.filter(
+            (interval) => interval.range[0] < nextInterval.range[0]
+        );
+
+        if (!filteredIntervals.length) {
+            return [nextInterval];
+        }
+
+        if (nextInterval.range[0] < _.last(filteredIntervals).range[1]) {
+            _.last(filteredIntervals).range[1] = nextInterval.range[0];
+        }
+
+        return [...filteredIntervals, nextInterval];
+    }, []);
+}
+
+function drillCommandToPlannedSlideVM({
+    endDepth,
+    startDepth,
+    toolface,
+    toolfaceMode,
+}) {
+    return {
+        range: [startDepth, endDepth],
+        toolface: {
+            val: toolface,
+            mode: toToolfaceModeVM(toolfaceMode),
+        },
+    };
+}
+
+function toToolfaceModeVM(toolfaceMode) {
+    return ["M", "G"][toolfaceMode];
+}
 
 async function getDataJson(path) {
     return (await fetch(path)).json();
@@ -11,7 +94,6 @@ async function getDataJson(path) {
 async function draw(recommendations) {
     const cw = COMMAND_WIDTH;
     let timePosition = 0;
-    let commandCount = 0;
     let toggle = true;
     let slideToggle = true;
 
@@ -38,7 +120,6 @@ async function draw(recommendations) {
                     drillC.endDepth - drillC.startDepth,
                     cw
                 );
-                commandCount++;
             });
         });
         drawText(
@@ -55,6 +136,23 @@ async function draw(recommendations) {
     });
 }
 
+async function drawSlides(slides) {
+    const cw = COMMAND_WIDTH * 4;
+    let toggle = true;
+
+    slides.forEach((sld) => {
+        toggle = !toggle;
+
+        ctxR.fillStyle = toggle
+            ? "rgb(0, 150, 136, 0.9)"
+            : "rgba(76, 175, 80, 0.9)";
+
+        console.log("SPAR:", sld.range[0], 0, sld.range[1] - sld.range[0], cw);
+
+        ctxR.fillRect(sld.range[0], 0, sld.range[1] - sld.range[0], cw);
+    });
+}
+
 function drawText(x, y) {
     ctx.fillStyle = "rgb(0, 0, 0)";
     ctx.font = "15px Arial";
@@ -67,9 +165,10 @@ function clearCanvas() {
 
 async function main(data) {
     draw(data.recommendations);
+    const slides = plannedSlidesFromRecommendations(data.recommendations);
+    console.log("slides", slides);
+    drawSlides(slides);
 }
-
-input.addEventListener("input", handleFiles, false);
 
 async function handleFiles() {
     const reader = new FileReader();
@@ -78,3 +177,9 @@ async function handleFiles() {
     };
     reader.readAsText(this.files[0]);
 }
+
+input.addEventListener("input", handleFiles, false);
+document.addEventListener("mousemove", (event) => {
+    console.log("clientX", event.clientX);
+    line.style.left = `${event.clientX}px`;
+});
